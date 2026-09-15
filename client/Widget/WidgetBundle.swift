9583b0fa65980e36efb51ce46b4c303c40574a45
gamepunk <17798615+gamepunk@users.noreply.github.com>
2026-09-15 14:53:39 +0800

    feat(client): 新增 Widget 扩展与 App 图标资源

---
+++ client/Widget/WidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by Billow on 2026/9/15.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Widget()
+        WidgetControl()
+    }
+}
